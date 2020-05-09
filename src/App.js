eb619e23692528dc92c44efcc67cca800acbcd6d
Md. Shafiq Khan <mdshafiq.ru@gmail.com>
2020-05-09 14:31:00 +0600

    created project with react app and added files for homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import HomePage from './pages/homepage/homepage'
 
 import './App.css';
 
 function App() {
   return (
     <div >
-      
+      <HomePage />
     </div>
   );
 }
